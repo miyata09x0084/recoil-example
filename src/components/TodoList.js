e35a977465050a133b6d035fccfc5772a74b81a9
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-10-03 12:44:36 +0900

    ただのatom値の取得のみから、atom状態の変化ができるように変える

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import { atom, selector, useRecoilValue } from 'recoil';
+import { useState } from 'react';
+import { useRecoilState } from 'recoil';
 import { todoListState } from '../atom';
 import TodoListStats from './TodoListStats';
 
 function TodoList() {
-    const todoList = useRecoilValue(todoListState);
+    const [title, setTitle] = useState('');
+    const [todoList, setTodoList] = useRecoilState(todoListState);
+
+    const handleChange = (e) => {
+        setTitle(e.target.value);
+    };
+
+    let id = 1;
+    function getId() {
+        return id++;
+    };
+
+    const addItem = () => {
+        setTodoList((oldTodoList) => [
+          ...oldTodoList,
+          {
+            id: getId(),
+            title: title,
+            isComplete: false,
+          },
+        ]);
+        setTitle('');
+    };
 
     return (
         <>
             <h1>RecoilによるTodoアプリ</h1>
             <TodoListStats />
+            <div>
+                <input type="text" value={title} onChange={handleChange} />
+                <button onClick={addItem}>Add</button>
+            </div>
             <ul>
                 {todoList.map((item) => (
                     <div key={item.id}>{item.title}</div>
