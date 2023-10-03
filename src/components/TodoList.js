63d905159ad69ecdaca602d26192031f5a7d8047
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-10-03 11:58:08 +0900

    initial commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,26 +1,22 @@
-import React from "react";
-import { useRecoilValue } from "recoil";
-import TodoListStats from "./TodoListStats";
-import TodoListFilters from "./TodoListFilters";
-import TodoItemCreator from "./TodoItemCreator";
-import TodoItem from "./TodoItem";
-import { filteredTodoListState } from "../recoil_state";
+import React from 'react';
+import { atom, selector, useRecoilValue } from 'recoil';
+import { todoListState } from '../atom';
+import TodoListStats from './TodoListStats';
 
-const TodoList = () => {
-  const todoList = useRecoilValue(filteredTodoListState);
-  return (
-    <>
-      <TodoListStats />
-      <div style={{ display: 'flex', gap: '10px' }}>
-        <TodoItemCreator />
-        <TodoListFilters />
-      </div>
-      {todoList.map((todoItem) => (
-        <TodoItem item={todoItem} key={todoItem.id} />
-      ))}
+function TodoList() {
+    const todoList = useRecoilValue(todoListState);
 
-    </>
-  );
-};
+    return (
+        <>
+            <h1>RecoilによるTodoアプリ</h1>
+            <TodoListStats />
+            <ul>
+                {todoList.map((item) => (
+                    <div key={item.id}>{item.title}</div>
+                ))}
+            </ul>
+        </>
+    );
+}
 
 export default TodoList;
