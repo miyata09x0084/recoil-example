4469e041bd4d377ab46342403d25405ab8f12065
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-10-04 10:11:57 +0900

    メモの削除と完了情報を共有状態的に管理する

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,11 +4,29 @@ import { todoListState } from '../atom';
 function TodoItem({ item }) {
     const [todoList, setTodoList] = useRecoilState(todoListState);
     const deleteItem = () => {
-        console.log('delete')
+        const index = todoList.findIndex((listItem) => listItem.id === item.id);
+        const newTodoList = [
+            ...todoList.slice(0, index),
+            ...todoList.slice(index + 1),
+        ];
+        setTodoList(newTodoList);
     };
+
+    const toggleItemCompletion = () => {
+        const index = todoList.findIndex((listItem) => listItem.id === item.id);
+        const newTodoList = [
+            ...todoList.slice(0, index),
+            { ...item, isComplete: !item.isComplete },
+            ...todoList.slice(index + 1),
+        ];
+        setTodoList(newTodoList);
+    }
 
 return (
     <div>
+        <button onClick={toggleItemCompletion}>
+            {item.isComplete ? '完' : '未'}
+        </button>
         {item.title}
         <span onClick={deleteItem} style={{cursor: 'pointer'}}>
             [x]
